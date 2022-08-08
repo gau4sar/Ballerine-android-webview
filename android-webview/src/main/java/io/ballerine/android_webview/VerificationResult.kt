84c8e2ebb88f3c7aa900c8e2575e0f0826d16e40
Gaurav Sarma <gaurav4sarma@gmail.com>
2022-08-08 20:34:47 +0530

    Initial commit

---
+++ android-webview/src/main/java/io/ballerine/android_webview/VerificationResult.kt
@@ -0,0 +1,11 @@
+package io.ballerine.android_webview
+
+import androidx.annotation.Keep
+
+@Keep
+data class VerificationResult(
+    val isSync: Boolean,
+    val status: String?,
+    val idvResult: String?,
+    val code: String?,
+)
